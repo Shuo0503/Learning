c08c3432ecce429e357a0b9672c68efaa3c2e4f8
Shuo Li <shuoli0503@gmail.com>
2024-08-06 18:15:41 -0700

    Palindrome Checker
    checks whether a given word is palindrome

---
+++ javascript/Palindrome Checker/script.js
@@ -0,0 +1,41 @@
+const checkBtn = document.getElementById("check-btn");
+const userInput = document.getElementById("text-input");
+const resultDiv = document.getElementById("result");
+
+
+
+const checkForPalindrome = input => {
+  const originalInput = input; // Store for later output
+
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
+
+  // Remove the previous result
+  resultDiv.replaceChildren();
+
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${originalInput}</strong> ${
+    lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+  } a palindrome.`;
+
+  const pTag = document.createElement('p');
+  pTag.innerHTML = resultMsg;
+  resultDiv.appendChild(pTag);
+
+  // Show the result.
+  resultDiv.classList.remove('hidden');
+};
+
+checkBtn.addEventListener('click', () => {
+  checkForPalindrome(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkForPalindrome(userInput.value);
+    userInput.value = '';
+  }
+});
